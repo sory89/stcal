0f5218f86eceb2f5a11c8591c046e683f446a6b7
Ricain <jeanmercadier@gmail.com>
2013-10-02 14:03:13 +0200

    mac

--- com/stcal/Fenetre.java
+++ com/stcal/Fenetre.java
@@ -1,6 +1,8 @@
 package com.stcal;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.Method;
 
 public class Fenetre extends JFrame {
 
@@ -11,6 +13,13 @@ public class Fenetre extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(new com.stcal.Panneau(prof,etu));
+        if (System.getProperty("os.name").contains("Mac")) {
+            mac(this);
+        }
+        JMenuBar jmb = new JMenuBar();
+        this.setJMenuBar(jmb);
+        JMenu file = new JMenu("File");
+        jmb.add(file);
     }
 
     /** rend la fenetre visible */
@@ -18,6 +27,21 @@ public class Fenetre extends JFrame {
         this.setVisible(true);
     }
 
+    private static void mac(Window window){
+        String className = "com.apple.eawt.FullScreenUtilities";
+        String methodName = "setWindowCanFullScreen";
 
+        try {
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            System.setProperty("com.apple.mrj.application.apple.menu.about.name","StCal");
+            Class<?> clazz = Class.forName(className);
+            Method method = clazz.getMethod(methodName, new Class<?>[] {Window.class, boolean.class });
+            method.invoke(null, window, true);
+        }
+        catch (Throwable t) {
+            System.err.println("Not compatible OSX");
+            t.printStackTrace();
+        }
+    }
 
 }
--- com/stcal/Main.java
+++ com/stcal/Main.java
@@ -3,17 +3,10 @@ package com.stcal;
 import java.lang.String;
 import java.io.IOException;
 
-
-
 public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-
-
-        com.Fenetre win = new com.Fenetre(com.Splitcsv.splitcsvprof("C:\\prof.csv"),com.Splitcsv.splitcsvetu("C:\\etu.csv"));
+        Fenetre win = new Fenetre(Splitcsv.splitcsvprof("prof.csv"),Splitcsv.splitcsvetu("etu.csv"));
         win.visible();
     }
-
-
-
 }

0736b32b995bd689d210bf2463248f3d519ae7a8
ValentinJ <valentin.jeanroy90000@laposte.net>
2014-01-22 10:30:50 +0100

    test

---
+++ com/stcal/don/DPersonne.java
@@ -0,0 +1,41 @@
+package com.stcal.don;
+
+import java.util.ArrayList;
+
+public class DPersonne{
+
+    protected ArrayList<String> info = new ArrayList<String>();
+
+    public DPersonne(){}
+
+	public DPersonne(String lastname, String firstname){
+		info.add(0,firstname);
+        info.add(1,lastname);
+	}
+
+    public void setInfo(String inf){
+        info.add(inf);
+    }
+
+    public ArrayList<String> getInfo(){
+        return info;
+    }
+
+	public String getNom()	{
+		return info.get(1);
+	}
+
+	public String getPrenom(){
+		return info.get(0);
+	}
+
+
+    public boolean is(String prenom,String nom){
+        return info.get(0).toLowerCase().equals(prenom.toLowerCase()) && info.get(1).toLowerCase().equals(nom.toLowerCase());
+    }
+
+    public String toString(){
+        return info.get(0) + " " + info.get(1);
+    }
+
+}

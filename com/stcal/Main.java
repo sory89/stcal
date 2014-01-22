f9071c15f2819a0d4a7440cddc2cc8785f6fe9e6
ValentinJ <valentin.jeanroy90000@laposte.net>
2014-01-22 10:17:05 +0100

    test

--- com/stcal/Main.java
+++ com/stcal/Main.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static final String ETU = "etu";
+    public static final String ETU =  "etu";
     public static final String PROF = "prof";
     public static final String NONE = "";
 

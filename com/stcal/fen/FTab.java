6dd78a9b6678e632304d4104076182ae67c33bb3
Ricain <jeanmercadier@gmail.com>
2014-01-28 14:49:00 +0100

    ajout com FTab

--- com/stcal/fen/FTab.java
+++ com/stcal/fen/FTab.java
@@ -9,24 +9,45 @@ public class FTab {
     private JPanel tabpan = new JPanel();
     private String nom = "";
 
+    /**
+     * Permet de creer un onglet
+     * @param titre: nom de l'ongelet
+     */
     public FTab(String titre){
         nom = titre;
         tabpan.setOpaque(false);
     }
 
+    /**
+     * Repaint et redessine l'ongelet
+     */
     public void refresh(){
         tabpan.revalidate();
         tabpan.repaint();
     }
 
+    /**
+     *
+     * @return JPanel de l'ongelet
+     */
     public JPanel pan(){
         return tabpan;
     }
 
+    /**
+     *
+     * @return Le nom de l'onglet
+     */
     public String getNom(){
         return nom;
     }
 
+    /**
+     * Chanque ongelet possede une boite de text pour afficher des infos.
+     * Cette methode permet de definir le texte a afficher.
+     * Elle est faite pour etre réecite dans les classes heritant de cette classe.
+     * @param details les chaines de caratere à afficher
+     */
     public void setInfo(ArrayList<String> details){}
 
 

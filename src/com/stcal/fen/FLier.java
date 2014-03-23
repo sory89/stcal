6a3776000ec2ae2f124563a99fc6b3a716508bd1
zdksd <ninjava@hotmail.fr>
2014-03-23 11:18:55 +0100

    Ajout de l'attribut diminutif dans la classe dprof. Il est impossible de lier un etudiant et un tuteur sans avoir mit de diminutif au tuteur. Je n'ai pas géré la base de donné .

--- src/com/stcal/fen/FLier.java
+++ src/com/stcal/fen/FLier.java
@@ -11,6 +11,8 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 public class FLier extends FTab {
@@ -19,6 +21,7 @@ public class FLier extends FTab {
     protected JPanel option= new JPanel();
     protected JList Fetu;
     protected JList Fprof;
+    protected JTextField Diminutif;
 
     public static JButton opt6 = new JButton("Valider");
 
@@ -32,6 +35,8 @@ public class FLier extends FTab {
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 askInfo(Fprof, Type.TUTEUR);
+                Diminutif.setEnabled(true);
+                afficherDiminutif();
             }
         });
         JPanel PanelProf = new JPanel();
@@ -75,7 +80,39 @@ public class FLier extends FTab {
 
         JScrollPane scrollPane = new JScrollPane(info);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        centre.add(scrollPane);
+        //centre.add(scrollPane);
+        JPanel A = new JPanel();
+        A.setLayout(new BorderLayout());
+        JPanel C = new JPanel();
+        JLabel B = new JLabel("Diminutif :");
+        Diminutif = new JTextField();
+        Diminutif.addKeyListener(new KeyListener() {
+
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                addDiminutif(Diminutif.getText());
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                addDiminutif(Diminutif.getText());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                addDiminutif(Diminutif.getText());
+            }
+        });
+
+        Diminutif.setPreferredSize(new Dimension(130,15));
+        C.add(B);
+        C.add(Diminutif);
+        Diminutif.setEnabled(false);
+        A.add(C, BorderLayout.NORTH);
+
+        A.add(scrollPane,BorderLayout.CENTER);
+        centre.add(A);
 
         pan().add(centre);
 
@@ -99,7 +136,24 @@ public class FLier extends FTab {
     }
 
 
+    protected void addDiminutif(String diminutif){
 
+        int i = Fprof.getSelectedIndex();
+        DProf p = (DProf)Datas.prof.get(i);
+        p.setDiminutif(diminutif);
+        Datas.prof.setElementAt(p,i);
+    }
+
+    protected void afficherDiminutif(){
+        String diminutif;
+        if(!Fprof.isSelectionEmpty()){
+            int i = Fprof.getSelectedIndex();
+            DProf p = (DProf)Datas.prof.get(i);
+            diminutif = p.getDiminutif();
+            Diminutif.setText(diminutif);
+
+        }
+    }
 
 
     protected void askInfo(JList<DPersonne> pan,Type type){
@@ -169,19 +223,42 @@ public class FLier extends FTab {
                 }
             });
             option.add(opt6);
-            Fetu.addListSelectionListener(new ListSelectionListener() {
-                @Override
-                public void valueChanged(ListSelectionEvent e) {
-                    opt6.setEnabled(!Fprof.isSelectionEmpty());
-                }
-            });
-
             Fprof.addListSelectionListener(new ListSelectionListener() {
                 @Override
                 public void valueChanged(ListSelectionEvent e) {
+
+                    opt6.setEnabled(Diminutif.getText()!=null && Diminutif.getText().length()>0 && !Fetu.isSelectionEmpty());
+
+                }
+            });
+            Fetu.addListSelectionListener(new ListSelectionListener() {
+                @Override
+                public void valueChanged(ListSelectionEvent e) {
+
+                    opt6.setEnabled(Diminutif.getText()!=null && Diminutif.getText().length()>0 && !Fprof.isSelectionEmpty());
+
+                }
+            });
+            Diminutif.addKeyListener(new KeyListener() {
+
+
+                @Override
+                public void keyTyped(KeyEvent e) {
+                    opt6.setEnabled(!Fetu.isSelectionEmpty());
+                }
+
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    opt6.setEnabled(!Fetu.isSelectionEmpty());
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e) {
                     opt6.setEnabled(!Fetu.isSelectionEmpty());
                 }
             });
+
+
         }
         refresh();
     }

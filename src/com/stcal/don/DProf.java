6c9a21596d2ea0160900ef3fb39465d28637806f
ValentinJ <valentin.jeanroy90000@laposte.net>
2014-03-23 13:45:56 +0100

    Ajout diminutif au tostring (correction)

--- src/com/stcal/don/DProf.java
+++ src/com/stcal/don/DProf.java
@@ -30,7 +30,9 @@ public class DProf extends DPersonne {
     }
 
     public String toString(){
-        return super.toString()+" ("+diminutif+")";
+        if(diminutif.equals(""))
+            return super.toString();
+        return super.toString()+" ["+diminutif+"]";
     }
 
 }
